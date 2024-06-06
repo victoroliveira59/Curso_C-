7889bf242524c5f76f24d9573fdda3c1991aedd9
Victor Oliveira <victorsamueloliveira59@gmail.com>
2024-06-06 14:04:47 -0300

    Arquivos da aula03

---
+++ Aulas/Aula03/aula03.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula03{
+    static void Main(){
+        int num = 0;
+        char letra = 'c';
+        float valor = 5.3f;
+        string nome = "Victor";
+
+        var aux = nome;
+        Console.Write(aux);
+        
+    }
+}
